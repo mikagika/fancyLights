67edf8c7237b3275307cfd51cabf4e9c6aa0ab71
Scott Chapman <scott@we3chapmans.com>
2022-01-11 00:25:14 -0500

    Make script handle start/stop

--- fancyLights.sh
+++ fancyLights.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-# sometimes it seems that we need to wait a few seconds for ws2812svr to get going
-sleep 5
-
-
 wdir="/home/pi/fancyLights"
 
-cat ${wdir}/start.ws >/dev/leds
-cat ${wdir}/rainbowRun.ws >/dev/leds
+case "$1" in
+  	start | up)
+		# sometimes it seems that we need to wait a few seconds for ws2812svr to get going
+		sleep 5
+
+		cat ${wdir}/start.ws >/dev/leds
+		cat ${wdir}/rainbowRun.ws >/dev/leds
+    ;;
+
+  	stop | down)
+		cat ${wdir}/off.ws >/dev/leds
+    ;;
+
+  	*)
+    	echo "Usage: $0 {start|stop|reload}"
+    ;;
+esac
+
 
